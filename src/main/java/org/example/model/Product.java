aaf166db8d65fe4e4a4a778d7f73d68437e4d39f
henkka <henrik.lappi@outlook.com>
2023-05-18 11:37:10 +0300

    Init project

---
+++ src/main/java/org/example/model/Product.java
@@ -0,0 +1,37 @@
+package org.example.model;
+
+public class Product {
+    private Long id;
+    private String name;
+    private int pieces;
+    public Product() {}
+    public Product(Long id, String name, int pieces) {
+        this.id = id;
+        this.name = name;
+        this.pieces = pieces;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPieces() {
+        return pieces;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPieces(int pieces) {
+        this.pieces = pieces;
+    }
+}
